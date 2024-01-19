6f70b0fef2c26bdd52f44c351622f7fe4d376884
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2024-01-19 17:57:53 +0000

    block styling

--- packages/filament-cms/resources/views/components/content-blocks.blade.php
+++ packages/filament-cms/resources/views/components/content-blocks.blade.php
@@ -1,5 +1,9 @@
-@props(['blocks' => [], 'nopadding' => false])
+@props(['blocks' => []])
 
+@if( count($blocks) > 0 )
+<div class="mt-20">
 @foreach($blocks as $block)
     @livewire($block['type'], ['blockData' => $block['data']])
 @endforeach
+</div>
+@endif
--- packages/filament-cms/resources/views/components/content-blocks/wrapper.blade.php
+++ packages/filament-cms/resources/views/components/content-blocks/wrapper.blade.php
@@ -1,10 +1,12 @@
 @props(['nopadding' => false, 'fullwidth' => false])
 
-<section
+<section class="odd:bg-gray-100">
+<div
     @class([
         "max-w-7xl mx-auto px-8" => ! $fullwidth,
-        "py-16" => ! $nopadding
+        "py-20" => ! $nopadding
     ])
 >
     {{  $slot }}
+</div>
 </section>
