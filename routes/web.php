124c55fa041cc4143852b02892a1c3679aa8cb7d
HUP234 Design <dave@hup234design.co.uk>
2024-11-27 13:52:50 +0000

    adding archive site

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,21 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/archive', 'archive/home');
 
-Route::get('/events/{slug}', [EventController::class, 'evnt'])->name('events.event');
-Route::get('/events', [EventController::class, 'index'])->name('events.index');
+Route::view('/', 'archive.home');
+Route::view('/elliots-story', 'archive.elliots-story');
+Route::view('/news-research', 'archive.news-research');
+Route::view('/support-us', 'archive.support-us');
+Route::view('/corporate-partners', 'archive.corporate-partners');
+Route::view('/contact', 'archive.contact');
+Route::view('/fundraising', 'archive.fundraising');
+Route::view('/forms', 'archive.forms');
 
-Route::get('/posts/{slug}', [PostController::class, 'post'])->name('posts.post');
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-
-Route::get('/{slug}', [PageController::class, 'page'])->name('pages.page');
-Route::get('/', [PageController::class, 'home'])->name('pages.home');
+//Route::get('/events/{slug}', [EventController::class, 'event'])->name('events.event');
+//Route::get('/events', [EventController::class, 'index'])->name('events.index');
+//
+//Route::get('/posts/{slug}', [PostController::class, 'post'])->name('posts.post');
+//Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+//
+//Route::get('/{slug}', [PageController::class, 'page'])->name('pages.page');
+//Route::get('/', [PageController::class, 'home'])->name('pages.home');
