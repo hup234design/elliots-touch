dedaeffe4fd2c6117be79676af82b943c4b53cf0
HUP234 Design <dave@hup234design.co.uk>
2024-11-27 16:03:18 +0000

    archive updates

--- resources/views/components/archive/layout.blade.php
+++ resources/views/components/archive/layout.blade.php
@@ -22,13 +22,28 @@
         }
     </script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     @stack('styles')
 </head>
 <body class="antialiased">
-<header class="max-w-6xl mx-auto px-4 lg:px-8 ">
+<header x-data="{ open: false }" class="relative max-w-6xl mx-auto px-4 lg:px-8 ">
+
+    <div class="flex items-center justify-between">
     <a href="{{ url('/') }}" class="block lg:w-3/4 mx-auto">
         <img class="w-full" src="{{ asset('archive/images/logo.png.webp') }}">
     </a>
+    <!-- Menu Button -->
+    <div class="p-4 lg:hidden">
+        <button
+            @click="open = !open"
+            class="text-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500"
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+            </svg>
+        </button>
+    </div>
+    </div>
     <nav class="hidden mt-4 px-8 lg:block">
         <ul class="flex justify-between items-center text-center">
             <li class="px-4">
@@ -54,6 +69,42 @@
 {{--            </li>--}}
         </ul>
     </nav>
+
+    <!-- Menu Items -->
+    <div
+        x-show="open"
+        @click.away="open = false"
+        x-transition:enter="transition ease-out duration-200"
+        x-transition:enter-start="opacity-0 transform scale-95"
+        x-transition:enter-end="opacity-100 transform scale-100"
+        x-transition:leave="transition ease-in duration-150"
+        x-transition:leave-start="opacity-100 transform scale-100"
+        x-transition:leave-end="opacity-0 transform scale-95"
+        class="absolute top-0 left-0 w-full bg-white shadow-md z-50"
+    >
+        <nav class="flex flex-col space-y-4 p-4">
+            <ul class="flex flex-col justify-between items-center text-center gap-4">
+                <li class="px-4">
+                    <a class="font-black text-lg" href="{{ url('/elliots-story') }}">Elliots Story</a>
+                </li>
+                <li class="px-4">
+                    <a class="font-black text-lg" href="{{ url('/news-research') }}">Research</a>
+                </li>
+                <li class="px-4">
+                    <a class="font-black text-lg" href="{{ url('/support-us') }}">Support Us</a>
+                </li>
+                <li class="px-4">
+                    <a class="font-black text-lg" href="{{ url('/corporate-partners') }}">Partners</a>
+                </li>
+                <li class="px-4">
+                    <a class="font-black text-lg" href="{{ url('/fundraising') }}">Fundraising</a>
+                </li>
+                <li class="px-4">
+                    <a class="font-black text-lg" href="{{ url('/contact') }}">Contact Us</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </header>
     <main class="max-w-6xl mx-auto px-4 lg:px-8 my-12">
         {{ $slot }}
