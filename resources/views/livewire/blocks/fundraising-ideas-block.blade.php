de419c689932beba28188322727a314c6d606400
HUP234 Design <dave@hup234design.co.uk>
2024-12-12 09:22:05 +0000

    WIP

--- resources/views/livewire/blocks/fundraising-ideas-block.blade.php
+++ resources/views/livewire/blocks/fundraising-ideas-block.blade.php
@@ -4,19 +4,19 @@
 
         @foreach( $this->ideas as $idea )
         <div
-            class="group flex flex-col overflow-hidden rounded-lg bg-white shadow-sm dark:bg-gray-800 dark:text-gray-100 hover:bg-gray-50"
+            class="group flex flex-col overflow-hidden rounded-lg bg-white dark:bg-gray-800 dark:text-gray-100"
         >
             <div class="w-full grow p-5 text-center lg:p-6">
                 <div class="-mx-2.5 -mt-2.5 mb-5 overflow-hidden rounded-md">
                     <a
                         href="javascript:void(0)"
-                        class="block transition duration-200 ease-out hover:scale-110 active:opacity-50"
+                        class="block"
                     >
-                        <div class="aspect-square bg-et-skyblue">
+                        <div class="aspect-square bg-et-skyblue rounded-full overflow-hidden">
                         <x-media-renderer
                             :data="$idea->featured_image"
                             :alt="$idea->title . ' Photo'"
-                            class="w-full h-full object-cover object-center"
+                            class="w-full h-full object-cover object-center rounded-full transition duration-200 ease-out group-hover:scale-105"
                         />
                         </div>
                     </a>
